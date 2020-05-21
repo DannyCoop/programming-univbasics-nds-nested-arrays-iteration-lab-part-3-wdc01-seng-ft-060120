cc4b69ee368aa6cb2eebe4070318af820a529c3e
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-05-21 13:10:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ while idx < src.length do
   inner_idx = 0
   while inner_idx < src[idx].length do
     if src[idx][inner_idx].class == String
-      final_string += src[idx][inner_idx] + ' '
+      final_string = final_string + src[idx][inner_idx] + ' '
     end
     inner_idx += 1
   end
